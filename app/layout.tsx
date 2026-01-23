a59bcdb6e622a2ddaec42c99cdee05b0bc28302f
tommy <tommyvong88@gmail.com>
2026-01-22 20:17:48 -0800

    Attempt to fix full height on mobile screen v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,11 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
